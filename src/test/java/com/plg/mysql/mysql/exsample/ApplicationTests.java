c70ee2b3b25641119fdd4128631c291c1d1c2fe9
Peter Bilberg <peter.langbo.bilberg.beumer@gmail.com>
2024-12-15 14:56:21 +0100

    a mysql exsamble

---
+++ src/test/java/com/plg/mysql/mysql/exsample/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plg.mysql.mysql.exsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
